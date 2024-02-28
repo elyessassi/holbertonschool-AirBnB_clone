00e80b722aca25d89cccc1c5c992286a597e42e9
elyessassi <mjoxen@gmail.com>
2024-02-28 11:35:35 +0100

    testing save instance method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,11 +3,12 @@
 
 from models.base_model import BaseModel
 import unittest
-from datetime import datetime
 
 
 class testcases(unittest.TestCase):
     """testing cases"""
     def testcase1(self):
+        """testing save"""
         test1 = BaseModel()
-        self.assertTrue(test1.save() != datetime.now())
+        test1.save()
+        self.assertTrue(test1.updated_at != test1.created_at)
