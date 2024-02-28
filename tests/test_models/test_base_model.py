3e635e97383ddd02ab68d91298cc8fb236634992
elyessassi <mjoxen@gmail.com>
2024-02-28 11:47:56 +0100

    testing __dict__ instance method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,3 +14,12 @@ class testcases(unittest.TestCase):
         test1.save()
         new = test1.updated_at
         self.assertTrue(old != new)
+    
+    def testcase2(self):
+        """testing __dict__"""
+        test1 = BaseModel()
+        dictionnary = test1.to_dict()
+        self.assertAlmostEqual(dictionnary["id"], test1.id)
+        self.assertAlmostEqual(dictionnary["created_at"], test1.created_at)
+        self.assertAlmostEqual(dictionnary["updated_at"], test1.updated_at)
+        self.assertAlmostEqual(dictionnary["__class__"], test1.__class__.__name__)
