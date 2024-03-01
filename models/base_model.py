e3289e95132f8915be2a2d02fc77709eaf99106e
elyessassi <mjoxen@gmail.com>
2024-03-01 23:26:47 +0100

    update basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initialise the id and the created_at and updated_at dates"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.isoformat(datetime.now())
+        self.updated_at = datetime.isoformat(datetime.now())
         if (kwargs is not None):
             format = "%Y-%m-%dT%H:%M:%S.%f"
             try:
@@ -37,6 +37,4 @@ class BaseModel:
         """show the instance attributes and class of an instance"""
         directory = self.__dict__
         directory["__class__"] = self.__class__.__name__
-        directory["created_at"] = datetime.isoformat(directory["created_at"])
-        directory["updated_at"] = datetime.isoformat(directory["updated_at"])
         return self.__dict__
