1853604f366bae7e1c8d6949e144899422cb3951
elyessassi <mjoxen@gmail.com>
2024-03-01 22:54:15 +0100

    update file storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,24 +6,24 @@ import os
 
 
 class FileStorage():
-    __file_path = ""
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """return the dictionary _objects"""
-        return self.__object
+        return self.__objects
 
     def new(self, obj):
         """add a new object to the dictionary"""
-        self.__objects[f"{self.__class__.__name__}.{self.id}"] = obj
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj.to_dict()
 
     def save(self):
         """save __objects to JSON file"""
-        with open(self.__filepath, "w") as f:
+        with open(self.__file_path, "w") as f:
             json.dump(self.__objects, f)
 
     def reload(self):
         """deserialize JSON file to __objects"""
         if os.path.isfile(self.__file_path) is True:
-            with open(self.__filepath, "r") as f:
+            with open(self.__file_path, "r") as f:
                 self.__objects = json.load(f)
