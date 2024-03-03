f296f4d42abaab839250fd400372240f5e1d7303
elyessassi <mjoxen@gmail.com>
2024-03-03 18:41:32 +0100

    test file for file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""testing cases ( file storage )"""
+
+from models.engine.file_storage import FileStorage
+import unittest
+import os
+
+class testcases(unittest.TestCase):
+	def testcase1(self):
+		testobj = FileStorage()
+		testobj.__file_path
+		self.assertTrue(os.path.isfile(testobj))
+		
