c5a25aaf0e4b4d2d509ae77643dd7b15bf35a6aa
elyessassi <mjoxen@gmail.com>
2024-03-03 23:50:12 +0100

    managing new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,6 +31,11 @@ class FileStorage():
         """deserialize JSON file to __objects"""
         from models.base_model import BaseModel
         from models.user import User
+        from models.city import City
+        from models.place import Place
+        from models.state import State
+        from models.review import Review
+        from models.amenity import Amenity
         
         if os.path.isfile(self.__file_path) is True:
             with open(self.__file_path, "r") as f:
