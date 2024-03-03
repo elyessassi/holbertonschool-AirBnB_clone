ab57f5315e814fac3f06993d60b192d330e38e24
elyessassi <mjoxen@gmail.com>
2024-03-03 23:49:32 +0100

    importing new classes

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ from models.base_model import BaseModel
 from models.user import User
 from models.engine.file_storage import FileStorage
 from models import storage
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
 
 
 classnames = ["BaseModel", "User"]
