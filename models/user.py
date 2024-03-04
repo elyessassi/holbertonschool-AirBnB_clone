a30805d070ad2288e38a0d2f8969c4fac9b63512
elyessassi <mjoxen@gmail.com>
2024-03-04 17:55:43 +0100

    pycodestyle

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""user class file"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+	"""user class file"""
 	email = ""
 	password = ""
 	first_name = ""
