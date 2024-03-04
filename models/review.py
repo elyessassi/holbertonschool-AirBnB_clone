18c42de419397232ba03c513cf5a3c8ad65758c5
elyessassi <mjoxen@gmail.com>
2024-03-04 17:53:58 +0100

    pycodestyle

--- models/review.py
+++ models/review.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""review class file"""
 
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
-	place_id = ""
-	user_id = ""
-	text = ""
+    """review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
