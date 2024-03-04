234497da0ddcb08e954c1943182b5e77212038a7
elyessassi <146719704+elyessassi@users.noreply.github.com>
2024-03-04 09:30:34 +0100

    Updating class names

--- console.py
+++ console.py
@@ -12,7 +12,7 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-classnames = ["BaseModel", "User"]
+classnames = ["BaseModel", "User"٫ "Review", "City", "Place","State", "Amenity"]
 
 class HBNBCommand(cmd.Cmd):
     """Cmd class"""
