cd5059d161d0880583c096a49b7869a0dd5fb849
elyessassi <mjoxen@gmail.com>
2024-03-04 17:52:18 +0100

    pycodestyle city

--- models/city.py
+++ models/city.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""amenty class file"""
 
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
-	state_id = ""
-	name = ""
+    """amenty class"""
+    state_id = ""
+    name = ""
