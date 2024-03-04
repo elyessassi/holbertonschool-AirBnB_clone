bba9a00b5cfab1b1a83ab7118d63cdc2923308bf
elyessassi <mjoxen@gmail.com>
2024-03-04 17:20:40 +0100

    updating base model test file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,7 +17,7 @@ class testcases(unittest.TestCase):
         new = test1.updated_at
         newstorage = storage.all().copy()
         self.assertTrue(old != new)
-        self.assertNotAlmostEqual(oldstorage ,newstorage)
+        self.assertNotAlmostEqual(len(oldstorage) ,len(newstorage))
 
     def testcase2(self):
         """testing __dict__"""
